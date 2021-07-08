97f80fc96fb6d4684691d573c6db9b4a596b8298
alexserediuc <alexserediuc99@gmail.com>
2021-07-08 09:15:09 +0300

    did something

--- app/src/main/java/com/alex/braintrainer/MainActivity.java
+++ app/src/main/java/com/alex/braintrainer/MainActivity.java
@@ -3,12 +3,26 @@ package com.alex.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button goButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goButton = findViewById(R.id.btn_go);
+    }
+
+    public void start(View view){
+        goButton.setVisibility(View.INVISIBLE);
+    }
+
+    public void choseAnswer(View view){
+
     }
 }
